2a9fee3bce60e5dd7d220a097dbd0e89408897a5
Pablo Rosales <phablore@MacBook-Pro-de-Pablo-Rosales.local>
2018-08-31 15:19:34 -0600

    cambios en proceso de tenido

--- app/detalle_tenido.php
+++ app/detalle_tenido.php
@@ -7,15 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class detalle_tenido extends Model
 {
     protected $table    = 'detalle_tenido';
-    protected $fillable = ['id_color',
+    protected $fillable = ['id_tenido',
                            'estado', 
                            'cantidad_tenida', 
                            'color',
                            'etapa',
                            'kilos',
-                           'quesos',
-                           'total_tenido',
-                           'receta_cantidad',
+                           'quesos'
                            ];
 
   public function tenido()
